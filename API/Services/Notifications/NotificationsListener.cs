6b071ba323fbd7c233e6b43fb8e679a84785966a
Julien Lavocat <julien.lavocat@gmail.com>
2021-02-19 02:05:07 +0100

    Moved notification parsing to its own method

--- API/Services/Notifications/NotificationsListener.cs
+++ API/Services/Notifications/NotificationsListener.cs
@@ -19,25 +19,27 @@ namespace Indiebackend.API.Services.Notifications
 
 			channel.Subscribe();
 
-			channel.OnMessage((channelName, rawData) =>
-			{
-				if (!(rawData is JObject))
-					return;
+			channel.OnMessage(ParseRawData);
+		}
 
-				JObject payload = (JObject) rawData;
-				string type = payload["type"].Value<string>();
-				JObject data = (JObject) payload["data"];
-				switch (type)
-				{
-					case "GROUP_DATA_UPDATED":
-						string groupId = ((JObject) data["group"])["id"].Value<string>();
-						OnNotification.Invoke(new  GroupUpdatedNotification(groupId));
-						break;
-					default:
-						OnNotification.Invoke(new BasicNotification(type));
-						break;
-				}
-			});
+		private void ParseRawData(string channelName, object rawData)
+		{
+			if (!(rawData is JObject))
+				return;
+
+			JObject payload = (JObject)rawData;
+			string type = payload["type"].Value<string>();
+			JObject data = (JObject)payload["data"];
+			switch (type)
+			{
+				case "GROUP_DATA_UPDATED":
+					string groupId = ((JObject)data["group"])["id"].Value<string>();
+					OnNotification.Invoke(new GroupUpdatedNotification(groupId));
+					break;
+				default:
+					OnNotification.Invoke(new BasicNotification(type));
+					break;
+			}
 		}
 	}
 }
